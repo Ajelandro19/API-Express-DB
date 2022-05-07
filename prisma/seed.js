3588212e09d642cc9b3e94261e8ea9b07ced309c
Ajelandro19 <almon190699@gmail.com>
2022-05-07 16:48:28 -0500

    Pasando linter al codigo

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,98 +1,98 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 (async function main() {
-  try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
-      update: {},
-      create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
-      },
-    });
+    try {
+        const woopa = await prisma.explorer.upsert({
+            where: { name: "Woopa" },
+            update: {},
+            create: {
+                name: "Woopa",
+                username: "ajolonauta",
+                mission: "Node"
+            },
+        });
 
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
-      update: {},
-      create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
-      },
-    });
+        const woopa1 = await prisma.explorer.upsert({
+            where: { name: "Woopa1" },
+            update: {},
+            create: {
+                name: "Woopa1",
+                username: "ajolonauta1",
+                mission: "Node"
+            },
+        });
 
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
-      update: {},
-      create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
-      },
-    });
+        const woopa2 = await prisma.explorer.upsert({
+            where: { name: "Woopa 2" },
+            update: {},
+            create: {
+                name: "Woopa 2",
+                username: "ajolonauta2",
+                mission: "Java"
+            },
+        });
 
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-    const hobi = await prisma.student.upsert({
-        where: { name: 'Hobi' },
-        update: {},
-        create: {
-          name: 'Hobi',
-                  lang: 'Español',
-                  missionCommander: 'Carlo',
-                  enrollments: 2
-        },
-      });
+        const woopa3 = await prisma.explorer.upsert({
+            where: { name: "Woopa 3" },
+            update: {},
+            create: {
+                name: "Woopa 3",
+                username: "ajolonauta3",
+                mission: "Node"
+            },
+        });
+        const hobi = await prisma.student.upsert({
+            where: { name: "Hobi" },
+            update: {},
+            create: {
+                name: "Hobi",
+                lang: "Español",
+                missionCommander: "Carlo",
+                enrollments: 2
+            },
+        });
   
-      const hobi1 = await prisma.student.upsert({
-        where: { name: 'Hobi1' },
-        update: {},
-        create: {
-          name: 'Hobi1',
-                    lang: 'Español',
-                    missionCommander: 'Fer',
-                    enrollments: 1,
-                    hasCertification: true
-        },
-      });
+        const hobi1 = await prisma.student.upsert({
+            where: { name: "Hobi1" },
+            update: {},
+            create: {
+                name: "Hobi1",
+                lang: "Español",
+                missionCommander: "Fer",
+                enrollments: 1,
+                hasCertification: true
+            },
+        });
   
-      const hobi2 = await prisma.student.upsert({
-        where: { name: 'Hobi3' },
-        update: {},
-        create: {
-                    name: 'Hobi3',
-                    lang: 'German',
-                    missionCommander: 'Carlo',
-                    enrollments: 4,
-                    hasCertification: true
-        },
-      });
+        const hobi2 = await prisma.student.upsert({
+            where: { name: "Hobi3" },
+            update: {},
+            create: {
+                name: "Hobi3",
+                lang: "German",
+                missionCommander: "Carlo",
+                enrollments: 4,
+                hasCertification: true
+            },
+        });
   
-      const hobi3 = await prisma.student.upsert({
-        where: { name: 'Hobi3' },
-        update: {},
-        create: {
-                    name: 'Hobi3',
-                    lang: 'Español',
-                    missionCommander: 'Fer'
-        },
-      });
+        const hobi3 = await prisma.student.upsert({
+            where: { name: "Hobi3" },
+            update: {},
+            create: {
+                name: "Hobi3",
+                lang: "Español",
+                missionCommander: "Fer"
+            },
+        });
 
-    console.log('Create 3 explorers');
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+        console.log("Create 3 explorers");
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
 })();
 
